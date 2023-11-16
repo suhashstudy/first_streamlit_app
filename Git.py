f05abc9a1b9fb6fbda27e534f7119e51ba2ff901
suhashstudy <150323814+suhashstudy@users.noreply.github.com>
2023-11-16 12:48:49 +0530

    Update Git.py
    New_code_changes

--- Git.py
+++ Git.py
@@ -1,34 +1,36 @@
 import streamlit as st
-import requests
+import subprocess
 
-def get_github_branches(username, repository, token):
-    # Construct the API URL for branches
-    url = f'https://api.github.com/repos/{username}/{repository}/branches'
-
-    # Set the Authorization header with the PAT
-    headers = {'Authorization': f'token {token}'}
-
-    # Make a GET request to the GitHub API
-    response = requests.get(url, headers=headers)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse and return the list of branches
-        branches = [branch['name'] for branch in response.json()]
-        return branches
-    elif response.status_code == 404:
-        st.error("Repository not found. Please check the username and repository name.")
-    else:
-        st.error(f"Failed to retrieve branches. Status code: {response.status_code}")
-        st.error(f"Response content: {response.content.decode('utf-8')}")
+def get_git_branches():
+    try:
+        # Run the git branch command and capture the output
+        result = subprocess.run(["git", "branch"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+        # Check if the command was successful
+        if result.returncode == 0:
+            # Split the output into lines and extract branch names
+            branches = [branch.strip() for branch in result.stdout.split('\n') if branch]
+            return branches
+        else:
+            # Display an error message if the command failed
+            st.error(f"Error retrieving Git branches:\n{result.stderr}")
+            return None
+    except Exception as e:
+        # Display any unexpected errors
+        st.error(f"An error occurred: {str(e)}")
         return None
 
-# Replace these values with your GitHub username, repository, and personal access token
-github_username = 'suhashstudy'
-github_repository = 'Git_task'
-github_token = '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'
+# Streamlit app
+def main():
+    st.title("Git Branch Viewer")
 
-branches = get_github_branches(github_username, github_repository, github_token)
+    # Get Git branches
+    branches = get_git_branches()
 
-if branches is not None:
-    st.success(f"Branches in {github_repository}: {branches}")
+    # Display branches in a Streamlit selectbox
+    if branches is not None:
+        selected_branch = st.selectbox("Select Git Branch", branches)
+        st.success(f"Selected branch: {selected_branch}")
+
+if __name__ == "__main__":
+    main()
