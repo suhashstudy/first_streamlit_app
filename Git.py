ed307f365fcad8e1fd21af83268ec8a2ec79a58d
suhashstudy <150323814+suhashstudy@users.noreply.github.com>
2023-11-16 14:39:23 +0530

    Update Git.py
    Used remote URL for accessing the repo

--- Git.py
+++ Git.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import git
 
-# Initialize the git repository
-repo = git.Repo('.')
+# Specify the remote URL of the repository
+remote_url = 'https://github.com/suhashstudy/Git_task.git'
+
+# Initialize the git repository from the remote URL
+repo = git.Repo.clone_from(remote_url, temp_dir=True)
 
 # Get a list of all branches
 branches = repo.heads
 
 # Display the list of branches in Streamlit
 st.header('Git Branches')
-if branches is not None:
-    selected_branch = st.selectbox("Select Git Branch", branches)
-    st.success(f"Selected branch: {selected_branch}")
+for branch in branches:
+    st.write(branch.name)
