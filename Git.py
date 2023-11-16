b10e2e4f77bc396d882b023fc3be221c2f375833
suhashstudy <150323814+suhashstudy@users.noreply.github.com>
2023-11-16 11:40:01 +0530

    Create Git.py
    Test_1(barnches list)

---
+++ Git.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import requests
+
+def get_github_branches(username, repository, token):
+    # Construct the API URL for branches
+    url = f'https://api.github.com/repos/{username}/{repository}/branches'
+
+    # Set the Authorization header with the PAT
+    headers = {'Authorization': f'token {token}'}
+
+    # Make a GET request to the GitHub API
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse and return the list of branches
+        branches = [branch['name'] for branch in response.json()]
+        return branches
+    elif response.status_code == 404:
+        st.error("Repository not found. Please check the username and repository name.")
+    else:
+        st.error(f"Failed to retrieve branches. Status code: {response.status_code}")
+        st.error(f"Response content: {response.content.decode('utf-8')}")
+        return None
+
+# Replace these values with your GitHub username, repository, and personal access token
+github_username = 'suhashstudy'
+github_repository = 'Git_task'
+github_token = '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'
+
+branches = get_github_branches(github_username, github_repository, github_token)
+
+if branches is not None:
+    st.success(f"Branches in {github_repository}: {branches}")
