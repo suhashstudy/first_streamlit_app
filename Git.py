dc44fe828d7b29c2fdf61a7c143d237789bf4b4e
suhashstudy <150323814+suhashstudy@users.noreply.github.com>
2023-11-16 15:24:25 +0530

    Update Git.py
    Git path and subprocess updated

--- Git.py
+++ Git.py
@@ -1,19 +1,39 @@
 import streamlit as st
-import git
+import subprocess
 
-# Specify the remote URL of the repository
-remote_url = 'https://github.com/suhashstudy/Git_task.git'
+def get_git_branches(repo_path):
+    try:
+        # Run the git branch command in the specified repository path
+        result = subprocess.run(["git", "branch"], cwd=repo_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+        # Check if the command was successful
+        if result.returncode == 0:
+            # Split the output into lines and extract branch names
+            branches = [branch.strip() for branch in result.stdout.split('\n') if branch]
+            return branches
+        else:
+            # Display an error message if the command failed
+            st.error(f"Error retrieving Git branches:\n{result.stderr}")
+            return None
+    except Exception as e:
+        # Display any unexpected errors
+        st.error(f"An error occurred: {str(e)}")
+        return None
 
-# Make a request to the Git API
-response = requests.get(remote_url + '/api/v4/projects/1/branches')
+# Streamlit app
+def main():
+    st.title("Git Branch Viewer")
 
-# Parse the JSON response
-branches = response.json()
+    # Specify the path to the Git repository
+    repo_path = "suhashstudy/Git_task"
 
-# Display the list of branches in Streamlit
-st.header('Git Branches')
-for branch in branches:
-    st.write(branch['name'])
+    # Get Git branches from the specified repository
+    branches = get_git_branches(repo_path)
 
+    # Display branches in a Streamlit selectbox
+    if branches is not None:
+        selected_branch = st.selectbox("Select Git Branch", branches)
+        st.success(f"Selected branch: {selected_branch}")
 
-
+if __name__ == "__main__":
+    main()
