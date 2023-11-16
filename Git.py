83d74029f5b00fe814005afb1ccd6c7a4b5c75c4
suhashstudy <150323814+suhashstudy@users.noreply.github.com>
2023-11-16 15:16:15 +0530

    Update Git.py
    remote url changes with api

--- Git.py
+++ Git.py
@@ -4,13 +4,16 @@ import git
 # Specify the remote URL of the repository
 remote_url = 'https://github.com/suhashstudy/Git_task.git'
 
-# Initialize the git repository from the remote URL
-repo = git.Repo.clone_from(remote_url, temp_dir=True)
+# Make a request to the Git API
+response = requests.get(remote_url + '/api/v4/projects/1/branches')
 
-# Get a list of all branches
-branches = repo.heads
+# Parse the JSON response
+branches = response.json()
 
 # Display the list of branches in Streamlit
 st.header('Git Branches')
 for branch in branches:
-    st.write(branch.name)
+    st.write(branch['name'])
+
+
+
